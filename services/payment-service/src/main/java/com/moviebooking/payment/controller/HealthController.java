a8c06596245712fd533e13cbd2e8a50c84e456c6
khoaanh4920 <khoaanh4920@gmail.com>
2026-01-23 00:54:52 +0700

    Init payment service

---
+++ services/payment-service/src/main/java/com/moviebooking/payment/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.moviebooking.payment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
